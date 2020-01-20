deb013e597a061bd31f237e75ae3ec1cb0311bc4
RichardHan0314 <shinystar0314@gmail.com>
2020-01-20 11:06:51 +0000

    added global style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { Router } from "react-router-dom";
 import Routes from "./routes/routes";
 import history from "./services/history";
 
+import GlobalStyle from "./styles/global";
+
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
