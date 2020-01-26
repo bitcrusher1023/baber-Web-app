9c704d1e6c4142574d2b97b8ee510de11e471cdd
RichardHan0314 <shinystar0314@gmail.com>
2020-01-26 12:06:53 +0000

    setup README.md file

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-BarberApp
+# BarberApp
+
+A barber app which has the functionality of allowing customers to book appointments for a barber shop.
+
+## Built With
+
+- HTML
+- CSS (Styled-Components)
+- JavaScript
+- React
+- Redux - Redux Saga
+- Node.js API
+
+Created using [create-react-app](https://github.com/facebook/create-react-app)
+
+### Backend
+
+BarberApp - Node.js [backend](https://github.com/danielferreiradf/barber_app)
+
+## Getting Started
+
+```
+npm install
+```
+
+Installs all necessary dependencies using node package manager.
+
+```
+npm start
+```
+
+Runs the app in development mode.
+
+```
+npm run build
+```
+
+Creates the final build of the project to be deployed on the server.
+
+## Images
+
+### SignIn Page
+
+![Image of SingIn](https://ibb.co/LYqGvwp)
+
+### SignUp Page
+
+![Image of SignUp](https://ibb.co/d0YYgbT)
+
+### Dashboard Page
+
+![Image of Dashboard](https://ibb.co/C6gdb5W)
+
+### Profile Page
+
+![Image of Profile](https://ibb.co/rskT2n3)
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,25 @@
-import React, { useState, useMemo } from "react";
-import { format, subDays, addDays } from "date-fns";
+import React, { useState, useMemo, useEffect } from "react";
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO
+} from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
 import pt from "date-fns/locale/pt";
 import api from "../../services/api";
 
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { Container, Time } from "./styles";
+import rangeAppointments from "../../services/rangeAppointments";
 
 export default function Dashboard() {
+  const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
 
   const formattedDate = useMemo(
@@ -20,6 +33,33 @@ export default function Dashboard() {
   const handleNextDay = () => {
     setDate(addDays(date, 1));
   };
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get("/schedule", {
+        params: { date }
+      });
+
+      // Gets users current time zone
+      const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = rangeAppointments.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timeZone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(appointment =>
+            isEqual(parseISO(appointment.date), compareDate)
+          )
+        };
+      });
+
+      setSchedule(data);
+    }
+    loadSchedule();
+  }, [date]);
 
   return (
     <Container>
@@ -37,25 +77,19 @@ export default function Dashboard() {
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Daniel F</span>
-        </Time>
-
-        <Time available>
-          <strong>09:00</strong>
-          <span>Daniel F</span>
-        </Time>
-
-        <Time>
-          <strong>10:00</strong>
-          <span>Daniel F</span>
-        </Time>
-
-        <Time>
-          <strong>11:00</strong>
-          <span>Daniel F</span>
-        </Time>
+        {schedule &&
+          schedule.map(time => (
+            <Time
+              key={time.time}
+              past={time.past}
+              available={!time.appointment}
+            >
+              <strong>{time.time}</strong>
+              <span>
+                {time.appointment ? time.appointment.user.name : "Em aberto"}
+              </span>
+            </Time>
+          ))}
       </ul>
     </Container>
   );
